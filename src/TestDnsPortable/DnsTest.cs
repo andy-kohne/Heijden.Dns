9d14873fc74aa71853cc8ebd2db3afe83abb1399
Benjamin Mayrargue <benjamin@vapolia.fr>
2016-11-23 18:39:56 +0100

    demo and appveyor

--- src/TestDnsPortable/DnsTest.cs
+++ src/TestDnsPortable/DnsTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Heijden.Dns.Portable;
 using Heijden.DNS;
@@ -8,68 +9,44 @@ namespace TestDnsPortable
 {
     public class DnsTest
     {
-        private readonly Resolver _resolver;
+        private readonly Resolver resolver;
 
         public DnsTest()
         {
-            _resolver = new Resolver();
-            _resolver.Recursion = true;
-            _resolver.UseCache = true;
-            //await _resolver.SetDnsServer("8.8.8.8"); // Google Public DNS
+            resolver = new Resolver
+            {
+                Recursion = true,
+                UseCache = true,
+                TimeOut = 1000,
+                Retries = 3,
+                TransportType = TransportType.Tcp,
+            };
 
-            _resolver.TimeOut = 1000;
-            _resolver.Retries = 3;
-            _resolver.TransportType = TransportType.Tcp;
+            //await _resolver.SetDnsServer("8.8.8.8"); // Google Public DNS
         }
 
         public async Task<IList<RecordSRV>> SrvRecords(string name)
         {
-            const QType qType = QType.SRV;
-            const QClass qClass = QClass.IN;
-
-            var response = await _resolver.Query(name, qType, qClass);
-
+            var response = await resolver.Query(name, QType.SRV);
             return response.RecordsSRV;
         }
 
         public async Task<IList<string>> CertRecords(string name)
         {
-            IList<string> records = new List<string>();
-            const QType qType = QType.CERT;
-            const QClass qClass = QClass.IN;
-
-            var response = await _resolver.Query(name, qType, qClass);
-            
-            foreach (var record in response.RecordsCERT)
-                records.Add(record.ToString());
-
-            return records;
+            var response = await resolver.Query(name, QType.CERT, QClass.IN);
+            return response.RecordsCERT.Select(record => record.ToString()).ToList();
         }
 
         public IList<string> GetQTypes()
         {
-            IList<string> items = new List<string>();
-            Array types = Enum.GetValues(typeof(QType));
-
-            for (int index = 0; index < types.Length; index++)
-            {
-                items.Add(types.GetValue(index).ToString());
-            }
-
-            return items;
+            var types = Enum.GetValues(typeof(QType));
+            return types.Cast<object>().Select((t, index) => types.GetValue(index).ToString()).ToList();
         }
 
         public IList<string> GetQClasses()
         {
-            IList<string> items = new List<string>();
-            Array types = Enum.GetValues(typeof(QClass));
-
-            for (int index = 0; index < types.Length; index++)
-            {
-                items.Add(types.GetValue(index).ToString());
-            }
-
-            return items;
+            var types = Enum.GetValues(typeof(QClass));
+            return types.Cast<object>().Select((t, index) => types.GetValue(index).ToString()).ToList();
         }
     }
 }
